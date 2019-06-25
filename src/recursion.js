39e2cdc97be5a61aefdca674e453daa99b8e7af8
Harrison Wu <harrisonywu@gmail.com>
2019-06-24 20:36:19 -0700

    Complete #13: Divide

--- src/recursion.js
+++ src/recursion.js
@@ -104,6 +104,9 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
 var divide = function(x, y) {
+  if (x === 0 || y === 0) return 0;
+  if (x===y) return 1;
+  return 1 + divide(x-y, y);
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
