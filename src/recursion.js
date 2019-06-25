cb6c31931bdf5e4659ef2d4228fffb613186336d
Harrison Wu <harrisonywu@gmail.com>
2019-06-24 18:51:43 -0700

    Complete #10: palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -79,6 +79,9 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string[0] != string[string.length - 1]) return false;
+  if (string.length <= 1) return true;
+  return palindrome(string.slice(1, string.length-1))
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
