16c2b00cf441ba462a8a1473da68bf5c05ba131c
Harrison Wu <harrisonywu@gmail.com>
2019-06-24 18:42:00 -0700

    Complete #7: exponent

--- src/recursion.js
+++ src/recursion.js
@@ -56,6 +56,8 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 1) return base;
+  return base * exponent(base, exp - 1)
 };
 
 // 8. Determine if a number is a power of two.
